8a68ffd245e6317be18937e918def4a584784bba
Vishal Sondarva <49022855+vishalsondarva@users.noreply.github.com>
2022-11-27 13:40:44 +0530

    2s

--- jmcurr-website/js/custom.js
+++ jmcurr-website/js/custom.js
@@ -6,4 +6,11 @@ $(document).ready(function () {
         if (scroll >= headerHeight) sticky.addClass('fixed');
         else sticky.removeClass('fixed');
     });
+
+    $("#menuBtn").click(function () {
+        $('#menuBtn .cross ').toggleClass('open')
+        $('body').toggleClass('oveHidden');
+        $("#menu").slideToggle(400);
+    });
+
 });
